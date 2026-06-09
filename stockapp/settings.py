011ba074946a9165e225a0f262dbf08271f90463
Pratham Saini <prathamsaini2005@gmail.com>
2026-06-09 14:34:17 +0530

    migrate django from sqlite to postgresql

--- stockapp/settings.py
+++ stockapp/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'stockapp.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fintech_ai",
+        "USER": "pratham",
+        "PASSWORD": "pratham123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
